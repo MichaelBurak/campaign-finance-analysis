b59827ff16007bcbf86287289bb2ebbe3420671d
MichaelBurak <michaelrburak@gmail.com>
2018-09-02 18:46:59 -0400

    Rand Forest Classification modeling and k-folds to compare

--- financeanalysis.py
+++ financeanalysis.py
@@ -42,12 +42,15 @@ nu_exp_2 = df['net_ope_exp'].isnull().sum()
 
 #drop all rows where contributions or expenses are null
 df = df.dropna(subset = ['net_con'])
-df = df.dropna(subset = ['net_ope_exp'])
+df = df.dropna(subset = ['cas_on_han_beg_of_per'])
+
 
 #filling in NaN of non winners with N and then turning  winner and net_con into numerical value
 df['winner'] = df['winner'].fillna('N')
 df['winner'] = df['winner'].eq('Y').mul(1)
 df['net_con'] = pd.to_numeric(df['net_con'])
+df['cas_on_han_beg_of_per'] = pd.to_numeric(df['cas_on_han_beg_of_per'])
+
 
 #separating out winners for viz
 #allwin = df[df.winner == 'Y']
@@ -60,7 +63,7 @@ df['net_con'] = pd.to_numeric(df['net_con'])
 #hwexpense = hwin['net_ope_exp']
 
 #Logistic regression modeling 
-X = df.iloc[:,[37]].values
+X = df.iloc[:,[35, 37]].values
 y = df.iloc[:, 43].values
 
 #train test split
@@ -89,20 +92,47 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-cm
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean() #.68
+accuracies.std() #.067
 
-'''viz train and test results
-Random forest modeling 
-from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 300)
-regressor.fit(X, y)
 
-Predicting a new result with random forest regressor
-y_pred = regressor.predict(6.5)
+#viz train and test results
 
-Gradient boosting modeling 
+#Random forest classification modeling
 
-Grid-search --> k folds cross validation model comparison 
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
 
-Clustering and cluster visualiation
-'''
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy')
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean() #.924
+accuracies.std() # .032
+
+#Gradient boosting modeling 
+
+#Grid-search --> k folds cross validation model comparison 
+
+#Clustering and cluster visualiation
