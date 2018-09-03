6dfe8ae0daa078413d72160b1e051620c53a0b4b
MichaelBurak <michaelrburak@gmail.com>
2018-09-02 20:46:54 -0400

    Adding XGBoost classifying

--- financeanalysis.py
+++ financeanalysis.py
@@ -57,17 +57,17 @@ df['cas_on_han_beg_of_per'] = pd.to_numeric(df['cas_on_han_beg_of_per'])
 #swin = allwin[allwin.can_off == "S"]
 #hwin = allwin[allwin.can_off == "H"]
 
-#expenses net of winning campaigns fir vuz
-#allwexpense = allwin['net_ope_exp']
-#swexpense = swin['net_ope_exp']
-#hwexpense = hwin['net_ope_exp']
+#net contributions of winning campaigns fir vuz
+#allwcon = allwin['net_con']
+#swcon = swin['net_con']
+#hwcon = hwin['net_con']
 
 #cash on hand for winning campaigns
 #allwexpense = allwin['cas_on_han_beg_of_per']
 #swexpense = swin['cas_on_han_beg_of_per']
 #hwexpense = hwin['cas_on_han_beg_of_per'] 
 
-#plots
+#plots and EDA 
 
 
 #Logistic regression modeling 
@@ -133,15 +133,37 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-#viz train and test results
-
 # Applying k-Fold Cross Validation
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 accuracies.mean() #.924
 accuracies.std() # .032
 
+#viz train and test results
+
+
 #Gradient boosting modeling (Xgboost?)
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting XGBoost to the Training set
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
 
 #Grid-search
 
